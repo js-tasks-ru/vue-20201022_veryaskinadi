7871174fa9f50e0084010549e85aeaf9f0cc1426
Dilyara <khakimovadi@gmail.com>
2020-11-16 11:24:16 +0300

    add solution for 03-vue-cli 02-FormGrop

--- 03-vue-cli/02-FormGroup/components/FormGroup.vue
+++ 03-vue-cli/02-FormGroup/components/FormGroup.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="form-group">
-    <label class="form-label">Label</label>
-    <!-- <input /> -->
+    <label v-if="label" class="form-label">{{ label }}</label>
+    <slot></slot>
+    <input :class = "inline ? 'form-group_inline' : ''" />
   </div>
 </template>
 
 <script>
 export default {
   name: 'FormGroup',
+
+  props: {
+    inline: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      type: String,
+    }
+  }
 };
 </script>
 
