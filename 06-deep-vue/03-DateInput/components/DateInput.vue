fc85375a8d40400ce2095defd673d1a7ada0b2c2
Dilyara <khakimovadi@gmail.com>
2021-01-28 20:33:32 +0300

    06-03

--- 06-deep-vue/03-DateInput/components/DateInput.vue
+++ 06-deep-vue/03-DateInput/components/DateInput.vue
@@ -1,5 +1,6 @@
 <template>
-  <AppInput>
+  <AppInput v-on="$listeners" v-bind="$attrs" :type="type">
+    
     <!-- Так можно передать все слоты в дочерний компонент -->
     <template v-for="slot of Object.keys($slots)" v-slot:[slot]>
       <slot :name="slot" />
@@ -18,6 +19,19 @@ export default {
   props: {
     // Чтобы value не было в списке $attrs, его можно указать в списке параметров
     value: {},
+    type: {
+      type: String,
+      default: 'date',
+      validator: function (value) {
+        return ['date', 'time', 'datetime-local'].indexOf(value) !== -1
+      }
+    },
+    valueAsNumber: {
+      type: Number,
+    },
+    valueAsDate: {
+      type: Date,
+    }
   },
 };
 </script>
